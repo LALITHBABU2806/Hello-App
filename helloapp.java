a944efceca0fdbadc47bb662d2f1bd3e13d9782d
Lalith Babu <lalithbabusingisetty@gmail.com>
2026-03-17 14:09:09 +0530

    uc4 done

--- helloapp.java
+++ helloapp.java
@@ -1,12 +1,20 @@
 public class helloapp {
-    public static void main (String[] args)  {
-        //Default name
-        String name = "World";
-        
-        //check if a line is provided as command line argument
-        if (args.length > 0) {
-            name = args[0]; //use the provided name
+    public static void main(String[] args) {
+
+        String message = "";
+
+        if (args.length == 0) {
+            message = "World";
+        } else {
+            for (int i = 0; i < args.length; i++) {
+                message += args[i];
+
+                if (i < args.length - 1) {
+                    message += ", ";
+                }
+            }
         }
-        System.out.println("Hello, " + name + "!");
+
+        System.out.println("Hello, " + message + "!");
     }
 }
